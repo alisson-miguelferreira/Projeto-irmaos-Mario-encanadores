688caf1e8b3a8a0b1e33b7a7212a8ecc95a2601b
alisson-miguelferreira <officealissonmiguelferreira@gmail.com>
2025-08-22 23:29:34 -0300

    correçoes de responsividade e padronização do header

--- scripts super Mario.js
+++ scripts super Mario.js
@@ -9,6 +9,7 @@ const mascara = document.querySelector(".mascara-formulario")
 
 function mostrarform (){
     
+    form.style.transition = "left 1.6s cubic-bezier(0.77, 0, 0.175, 1)";
     form.style.left ="50%"
     form.style.transform ="translateX(-50%)"
     mascara.style.visibility="visible"
@@ -16,6 +17,7 @@ function mostrarform (){
 
 function esconderform (){
 
+    form.style.transition = "left 1.6s cubic-bezier(0.77, 0, 0.175, 1)";
     form.style.left ="-300px"
     form.style.transform = "translateX(0)"
     mascara.style.visibility="hidden"
